2abb7b88559de6e6fe0d903bec2ffcca2c1ebb18
haochen <haochen1204@aliyun.com>
2023-11-05 22:02:52 +0800

    add fofa_api group

---
+++ WaitGroupCount.go
@@ -0,0 +1,44 @@
+package HCGOTools
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type WaitGroupCount struct {
+	sync.WaitGroup
+	NowCount int64
+	MaxCount int64
+}
+
+func NewWaitGroupCount(count *int64) WaitGroupCount {
+	var NowCount = 1
+	var MaxCount = atomic.LoadInt64(count)
+	return WaitGroupCount{NowCount: int64(NowCount), MaxCount: MaxCount}
+}
+
+func (wg *WaitGroupCount) Add(delta int) {
+	atomic.AddInt64(&wg.NowCount, int64(delta))
+	wg.WaitGroup.Add(delta)
+}
+
+func (wg *WaitGroupCount) WaitThread() bool {
+	for {
+		if int64(wg.GetCount()) >= wg.MaxCount {
+			//fmt.Println("等待中...", wg.NowCount, wg.MaxCount)
+			time.Sleep(1)
+		} else {
+			return true
+		}
+	}
+}
+
+func (wg *WaitGroupCount) Done() {
+	atomic.AddInt64(&wg.NowCount, -1)
+	wg.WaitGroup.Done()
+}
+
+func (wg *WaitGroupCount) GetCount() int {
+	return int(atomic.LoadInt64(&wg.NowCount))
+}
---
+++ fofa_api.go
@@ -0,0 +1,96 @@
+package HCGOTools
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const BaseURL = "https://api.fofa.info"
+
+type FoFa_Client struct {
+	email  string
+	apiKey string
+}
+
+type FoFa_APIInfo struct {
+	Error      bool   `json:"error"`
+	Email      string `json:"email"`
+	UserName   string `json:"username"`
+	Fcoin      int    `json:"fcoin"`
+	Isvip      bool   `json:"isvip"`
+	VipLevel   int    `json:"vip_level"`
+	IsVerfied  bool   `json:"is_verified"`
+	Avatar     string `json:"avatar"`
+	Message    string `json:"message"`
+	FofaCliVer string `json:"fofacli_ver"`
+	FofaServer bool   `json:"fofa_server"`
+}
+
+type FoFa_Host struct {
+	Error   bool       `json:"error"`
+	Size    int        `json:"size"`
+	Page    int        `json:"page"`
+	Mode    string     `json:"mode"`
+	Query   string     `json:"query"`
+	Results [][]string `json:"results"`
+}
+
+type FoFa_InfoSearch struct {
+	Qbase64 string
+	Fields  string
+	Page    int
+	Size    int
+	Full    bool
+}
+
+func New_FoFa_Client(email string, apiKey string) *FoFa_Client {
+	return &FoFa_Client{email: email, apiKey: apiKey}
+}
+
+func New_FoFa_InfoSearch(q string) *FoFa_InfoSearch {
+	q = base64.StdEncoding.EncodeToString([]byte(q))
+	p := FoFa_InfoSearch{Qbase64: q, Fields: "no", Page: 0, Size: 0, Full: false}
+	return &p
+}
+
+func (s *FoFa_Client) HostSearch(q *FoFa_InfoSearch) (*FoFa_Host, error) {
+	api_url := fmt.Sprintf("%s/api/v1/search/all?email=%s&key=%s&qbase64=%s", BaseURL, s.email, s.apiKey, q.Qbase64)
+	if q.Fields != "no" {
+		api_url = api_url + fmt.Sprintf("&fields=%s", q.Fields)
+	}
+	if q.Page != 0 {
+		api_url = api_url + fmt.Sprintf("&page=%d", q.Page)
+	}
+	if q.Size != 0 {
+		api_url = api_url + fmt.Sprintf("&size=%d", q.Page)
+	}
+	if q.Full != false {
+		api_url = api_url + "&full=ture"
+	}
+	res, err := http.Get(api_url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var ret FoFa_Host
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, err
+}
+
+func (s *FoFa_Client) APIInfo() (*FoFa_APIInfo, error) {
+	res, err := http.Get(fmt.Sprintf("%s/api/v1/info/my?email=%s&key=%s", BaseURL, s.email, s.apiKey))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var ret FoFa_APIInfo
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
